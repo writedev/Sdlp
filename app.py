72ee961a1b55c7ea29c6ae72879ef00353b70749
writedev <locxistwitch@gmail.com>
2026-02-08 13:10:49 +0100

    feat: add mov & mkv format but with warning because this take time | fix: change format opts structs | feat: add a name file option

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from typer import Typer
 from enum import StrEnum
 import typer
-from yt_dlp import YoutubeDL, postprocessor
-from typing import Annotated, Optional, Literal, Union
+from yt_dlp import YoutubeDL
+from typing import Annotated
 import random
 from rich.prompt import Prompt, Confirm
 from rich.console import Console
@@ -19,6 +19,8 @@ class AudioFormat(StrEnum):
 
 class VideoFormat(StrEnum):
     MP4 = "mp4"
+    MOV = "mov"
+    MKV = "mkv"
 
 
 class EveryFormat(StrEnum):
@@ -28,6 +30,28 @@ class EveryFormat(StrEnum):
     M4A = "m4a"
     # Video Format
     MP4 = "mp4"
+    MOV = "mov"
+    MKV = "mkv"
+
+
+re_encode_opts = {
+    "postprocessor_args": {
+        "ffmpeg": [
+            "-c:v",
+            "libx264",
+            "-pix_fmt",
+            "yuv420p",
+            "-profile:v",
+            "high",
+            "-level",
+            "4.2",
+            "-c:a",
+            "aac",
+            "-b:a",
+            "192k",
+        ]
+    },
+}
 
 
 @app.command()
@@ -38,11 +62,15 @@ def main():
 @app.command()
 def download(
     format: EveryFormat,
+    file_name: Annotated[
+        str, typer.Option(help="Choose file folder name")
+    ] = "%(title)s",
     worst: Annotated[bool, typer.Option(help="Get the worst quality video")] = False,
     random_number: Annotated[
         bool, typer.Option(help="Remove the random number in the end folder name")
     ] = True,
 ):
+
     url: str = Prompt.ask("[b]What is the url ? 🔗 [/b]")
     if not url.startswith("https://"):
         raise typer.Exit()
@@ -60,35 +88,43 @@ def download(
         }
 
     if format.value in VideoFormat:
-        if worst:
+        if not worst:
             format_opts = {
-                # "format": "bv*[ext=mp4]+ba[ext=m4a]/b[ext=mp4]/b",
-                "format": "worstvideo*+worstaudio/worst",
+                "format": "bestvideo+bestaudio/best",
                 "merge_output_format": format.value,
             }
         else:
             format_opts = {
                 # "format": "bv*[ext=mp4]+ba[ext=m4a]/b[ext=mp4]/b",
-                # "format": "bv*[vcodec^=avc1]+ba[acodec^=mp4a]/b",
-                "format": "bestvideo*+bestaudio/best",
+                "format": "worstvideo*+worstaudio/worst",
                 "merge_output_format": format.value,
             }
 
+        if format.value == "mov" or format.value == "mkv":
+            format_confirmation = Confirm.ask(
+                "[bold red] Are you sure you want to use mov or mkv because this will require re encored entire video and will take time and performance ? [/bold red]"
+            )
+
+            if not format_confirmation:
+                console.print("Good decision to save time.")
+                raise typer.Exit()
+
+            format_opts = format_opts | re_encode_opts  # Add recoding options
+
     if random_number:
         title_opts = {
-            "outtmpl": f"./%(title)s |[{random.randint(1, 1000)}].%(ext)s",
+            "outtmpl": f"./{file_name} |[{random.randint(1, 1000)}].%(ext)s",
             "download_archive": None,
             "force_overwrites": True,
         }
     else:
         title_opts = {
-            "outtmpl": "./%(title)s.%(ext)s",
-            "postprocessor_args": ["-loglevel", "error"],
+            "outtmpl": f"./{file_name}.%(ext)s",
         }
 
-    ydl_opts = format_opts | title_opts
+    ydl_opts = format_opts | title_opts  # type: ignore
 
-    with YoutubeDL(ydl_opts) as ydl:
+    with YoutubeDL(ydl_opts) as ydl:  # type: ignore
         ydl.download(url)
 
 
