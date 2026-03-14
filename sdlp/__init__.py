242a4a5cb5052ed1badd655591897526106ab1e7
writedev <locxistwitch@gmail.com>
2026-03-14 15:56:26 +0100

    feat: add command to display ytdlp and sdlp version

--- sdlp/__init__.py
+++ sdlp/__init__.py
@@ -1,11 +1,22 @@
 from .core import app as core_app
 import typer
+from rich.console import Console
+import yt_dlp.version as ydl_version
 
 __version__ = "2.1"
 
 app = typer.Typer(suggest_commands=True)
+console = Console()
+
 
 app.add_typer(core_app)
+
+
+@app.command()
+def version():
+    console.print(f"Sdlp version: [b]{__version__}[/b]")
+    console.print(f"Sdlp version: [b]{ydl_version.__version__}[/b]")
+
 
 __main__ = app()
 
