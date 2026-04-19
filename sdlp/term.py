5f9529932c40afe32d5a9299a80a719df94215da
writedev <locxistwitch@gmail.com>
2026-04-19 22:00:49 +0200

    feat: add audio command

--- sdlp/term.py
+++ sdlp/term.py
@@ -7,7 +7,7 @@ from rich.prompt import Prompt
 import sdlp
 
 from .ui.progress_hooks import progress_downloading, spinner_postprocess
-from .utils.format import VideoFormat
+from .utils.format import AudioFormat, VideoFormat
 
 app = typer.Typer()
 console = Console(markup=True, emoji=True)
@@ -28,7 +28,7 @@ def main():
 
 @app.command()
 def video(
-    url: str = "",
+    url: Annotated[str, typer.Argument()] = "",
     format: Annotated[VideoFormat, typer.Option()] = VideoFormat.MP4,
 ):
     if not url:
@@ -39,6 +39,24 @@ def video(
 
     try:
         sdlp.download_video(url=url, format=format.value, extras=(ui_opts))
+
+    except Exception as e:
+        print(e)
+
+
+@app.command()
+def audio(
+    url: Annotated[str, typer.Argument()] = "",
+    format: Annotated[AudioFormat, typer.Option()] = AudioFormat.MP3,
+):
+    if not url:
+        url = Prompt.ask(
+            "[b]Enter the url of your video for transfrom its in audio[/b]",
+            console=console,
+        )
+
+    try:
+        sdlp.download_audio(url=url, format=format.value, extras=(ui_opts))
 
     except Exception as e:
         print(e)
